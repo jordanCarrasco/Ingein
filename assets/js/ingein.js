79736a06410e20a2d1001054263219fa16cd33f8
Your Name <jordan.carrasco.d@gmail.com>
2016-06-24 14:59:59 -0400

    cambios en js ingein

--- assets/js/ingein.js
+++ assets/js/ingein.js
@@ -1,16 +1,114 @@
 $(document).ready(function(){
-	$('#paginaCargada').hide();
+	//$('#paginaCargada').hide();
 	$.ajax({
 		url:'administration/dataPage.json',
 		type: 'GET',
 		dataType:'json',
 		success: function(result){
 			$.each(result,function(i,item){
-                alert(item);
+                createHtmlSection(item);
                         
             });
 		}
 	});
 
 
-})
+});
+
+
+function createHtmlSection(item){
+	var section = item.tipoSection;
+	var html = "";
+	switch(section){
+		case 'quienes_somos':
+			html = "<div class='col-md-6 col-md-offset-3 wow fadeIn'>" +
+				   " <h1>"+ item.titulo+"</h1>" +
+                   " <hr class='colored'>"  +                   
+                   "</div>" +
+                   "<div class='row'>" +
+                   "<div class='col-md-6 col-md-offset-3'  align='justify'>" +
+                   " <p>" + item.desc + "</p>" +
+                   "</div></div>";
+                   $('#sectionAbout').html(html); 
+			break;
+		case 'nuestro_equipo':
+		/*var htmlAux = "";
+			html = "<div class='row'>"+
+		            "    <div class='col-lg-12'>"+
+		            "        <h2>"+item.titulo +"</h2>"+
+		            "        <p>"+item.subtitulo +"</p>"+
+		            "        <hr class='colored'>"+
+		            "    </div>"+
+		            "</div>"+
+		            "<div class='row content-row'>"+
+		            "    <div class='col-lg-12'>"+
+		            "        <div class='about-carousel'>";
+		            $.each(item.integrantes,function(i,item){
+		              htmlAux = htmlAux +"<div class='item'>"+
+		            "                <img src='assets/img/people/1.jpg' class='img-responsive' alt=''>"+
+		            "                <div class='caption'>"+
+		            "                    <h3>"+item.nombre +"</h3>"+
+		            "                    <hr class='colored'>"+
+		            "                    <p>"+item.cargo +"</p>"+
+		            "                    <ul class='list-inline social'>"+
+		            "                        <li>"+
+		            "                            <a href='#'><i class='fa fa-facebook fa-fw'></i></a>"+
+		            "                        </li>"+
+		            "                        <li>"+
+		            "                            <a href='#'><i class='fa fa-twitter fa-fw'></i></a>"+
+		            "                        </li>"+
+		            "                        <li>"+
+		            "                            <a href='#'><i class='fa fa-linkedin fa-fw'></i></a>"+
+		            "                        </li>"+
+		            "                    </ul>"+
+		            "                </div>"+
+		            "            </div>"; 
+		            });	      
+		                        
+		            html = html + htmlAux + "</div>"+
+		            "    </div>"+
+		            "</div>";
+		            $('#nuestroEquipo').html(html);*/
+		            
+		break;
+		case 'nuestros_servicios':
+			var aux = "";
+			var aux2 = "";
+			html = 	"<div class='row text-center'>" +
+            "    <div class='col-lg-12 wow fadeIn'>" +
+            "        <h2>"+item.titulo+"</h2>" +
+            "        <hr class='colored'>" +
+            "        <p>"+item.subtitulo+"</p>" +
+            "    </div>" +
+            "</div>" +
+            "<div class='row content-row'>";
+            $.each(item.desc,function(i,b){
+            	aux = aux + 
+            	"    <div class='col-md-4 wow fadeIn' data-wow-delay='.2s'>" +
+	            "        <div class='media'>" +
+	            "            <div class='pull-left'>" +
+	            "                <i class='fa fa-clipboard'></i>" +
+	            "            </div>" +
+	            "            <div class='media-body'>" +
+	            "                <h3 class='media-heading'>"+b.titulo+"</h3>"+
+	            "                <ul>" +
+	            $.each(b.detalle,function(i,details){
+	            	aux2 = aux2 + "<li>"+details.desc+"</li>";
+
+	            });
+	            aux = aux + aux2;
+	            aux = aux + "            </div>" +
+				            "        </div>" +
+				            "    </div>";
+            });          
+                   
+            "</div>" ;
+
+
+		break;
+		case 'nuestro_trabajo':
+		break;		
+
+	}
+
+}
